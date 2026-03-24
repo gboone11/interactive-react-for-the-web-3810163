992794e84b5f04177eacb154dc1b24cfb3d18cc5
gboone11 <gavin.boone@ejgallo.com>
2026-03-24 15:41:52 -0700

    Ch02 06

--- Ch02/wip/app.jsx
+++ Ch02/wip/app.jsx
@@ -3,7 +3,7 @@
     <div className="field-group">
       <label htmlFor="size-options">Size:</label>
       <select defaultValue={props.size} name="sizeOptions" id="size-options">
-        {window.Inventory.allSizes.map((num) => (
+        {props.sizes.map((num) => (
           <option values={num} key={num}>
             {num}
           </option>
@@ -16,16 +16,22 @@
     <img src={`../../assets/${props.color}.jpg`} alt={`A ${props.color} sneaker`} />
   );
 
-  const ProductCustomizer = () => (
-    <div className="customizer">
-      <div className="product-image">
-        <ProductImage color="red" />
+  const ProductCustomizer = () => {
+    const [size, setSize] = React.useState(8);
+    const [sizes, setSizes] = React.useState(window.Inventory.allSizes);
+    const [color, setColor] = React.useState("red");
+
+    return (
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage color={color} />
+        </div>
+        <div className="selectors">
+          <SizeSelector size={size} sizes={sizes} />
+        </div>
       </div>
-      <div className="selectors">
-        <SizeSelector size={8} />
-      </div>
-    </div>
-  );
+    );
+  };
 
   const root = ReactDOMClient.createRoot(document.getElementById("react-root"));
   root.render(<ProductCustomizer />);
