1ce86e102afcaf5224c92f753474b3796ec5d7a7
gboone11 <gavin.boone@ejgallo.com>
2026-03-24 14:52:36 -0700

    Ch02 03

--- Ch02/03/app.jsx
+++ Ch02/03/app.jsx
@@ -1,3 +1,16 @@
 (() => {
-	// Start here
+  const ProductImage = () => <img src="../../assets/red.jpg" alt="A red sneaker" />;
+
+  const ProductCustomizer = () => {
+    return (
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage />
+        </div>
+      </div>
+    );
+  };
+
+  const root = ReactDOMClient.createRoot(document.getElementById("react-root"));
+  root.render(<ProductCustomizer />);
 })();
