7e820c7c6fae56996f0fa77bce45ad22d6ea5e47
gboone11 <gavin.boone@ejgallo.com>
2026-03-24 14:31:00 -0700

    Ch2 01

--- Ch02/01/tinyapp.js
+++ Ch02/01/tinyapp.js
@@ -1,3 +1,10 @@
 (() => {
-	// Start here
+  const productCustomizer = React.createElement(
+    "div",
+    { className: "customizer" },
+    "Product Customizer will go here",
+  );
+
+  const root = ReactDOMClient.createRoot(document.getElementById("react-root"));
+	root.render(productCustomizer)
 })();
