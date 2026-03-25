388bcfefdd4aa5b9ce33810f1f5cc72ccda2cb3f
gboone11 <gavin.boone@ejgallo.com>
2026-03-25 15:08:10 -0700

    Ch04 01-04

--- Ch04/wip/hotel.jsx
+++ Ch04/wip/hotel.jsx
@@ -1,119 +1,111 @@
-import React, { StrictMode, Fragment, useState } from "react";
+import React, { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import date from "date-and-time";
 
 const messageTypes = {
-	management: "Management",
-	dining: "Dining Services",
-	ops: "Operations",
-	plumbing: "Plumbing",
-	pool: "Pool",
+  management: "Management",
+  dining: "Dining Services",
+  ops: "Operations",
+  plumbing: "Plumbing",
+  pool: "Pool",
 };
 
 const apiUrl = "/api";
 
 function PostForm(props) {
-	const typeOptions = Object.keys(messageTypes).map(function (key) {
-		return (
-			<option key={key} value={key}>
-				{messageTypes[key]}
-			</option>
-		);
-	});
+  const typeOptions = Object.keys(messageTypes).map(function (key) {
+    return (
+      <option key={key} value={key}>
+        {messageTypes[key]}
+      </option>
+    );
+  });
 
-	// so we don't have to type this over and over
-	const defaultType = typeOptions[0].key;
+  // so we don't have to type this over and over
+  const defaultType = typeOptions[0].key;
 
-	return (
-		<form>
-			<h3>Post an Update</h3>
+  return (
+    <form>
+      <h3>Post an Update</h3>
 
-			<div className="field-group">
-				<label htmlFor="txt-message">Message</label>
-				<textarea id="txt-message" rows="2" required />
-			</div>
+      <div className="field-group">
+        <label htmlFor="txt-message">Message</label>
+        <textarea id="txt-message" rows="2" required />
+      </div>
 
-			<div className="field-group">
-				<label htmlFor="txt-type">Type</label>
-				<select id="txt-type">{typeOptions}</select>
-			</div>
+      <div className="field-group">
+        <label htmlFor="txt-type">Type</label>
+        <select id="txt-type">{typeOptions}</select>
+      </div>
 
-			<div className="field-group action">
-				<input type="submit" value="Post Update" />
-			</div>
-		</form>
-	);
+      <div className="field-group action">
+        <input type="submit" value="Post Update" />
+      </div>
+    </form>
+  );
 }
 
 function StatusMessage(props) {
-	const statusDate = date.parse(props.time, "YYYY-MM-DD HH:mm:ss");
-	const dateFormat = "M/D/Y, h:mm A";
+  const statusDate = date.parse(props.time, "YYYY-MM-DD HH:mm:ss");
+  const dateFormat = "M/D/Y, h:mm A";
 
-	return (
-		<div className="status-message">
-			{props.msg}
-			<span className="name">— {props.type}</span>
-			<span className="time">{date.format(statusDate, dateFormat)}</span>
-		</div>
-	);
+  return (
+    <div className="status-message">
+      {props.msg}
+      <span className="name">— {props.type}</span>
+      <span className="time">{date.format(statusDate, dateFormat)}</span>
+    </div>
+  );
 }
 
 function StatusMessageList(props) {
-	const stubStatuses = [
-		{
-			id: 1,
-			msg: "The hot tub is currently closed for maintenance.  We expect it to be back up and running within 48 hours.",
-			type: "management",
-			time: "2025-04-11 09:15:32",
-		},
-		{
-			id: 2,
-			msg: "The hot tub maintenance is complete.  Please enjoy a dip!",
-			type: "management",
-			time: "2025-04-14 17:12:08",
-		},
-		{
-			id: 3,
-			msg: "The rice cooker is on the fritz, any fried rice dishes will require some extra time to cook.",
-			type: "dining",
-			time: "2025-04-18 15:00:00",
-		},
-	];
+  const [statuses, setStatuses] = useState(stubStatuses);
 
-	const [statuses, setStatuses] = useState(stubStatuses);
+  useEffect(() => {
+    const abortController = new AbortController();
 
-	let displayedStatuses;
+    fetch("/api/messages", { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => setStatuses(data))
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          console.error("Error fetching from /api/messages", error);
+        }
+      });
 
-	if (statuses.length > 0) {
-		displayedStatuses = statuses.map((status) => {
-			return (
-				<li key={status.id} className={status.type}>
-					<StatusMessage
-						msg={status.msg}
-						type={messageTypes[status.type]}
-						time={status.time}
-					/>
-				</li>
-			);
-		});
-	}
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
-	return <ul id="status-list">{displayedStatuses}</ul>;
+  let displayedStatuses;
+
+  if (statuses.length > 0) {
+    displayedStatuses = statuses.map((status) => {
+      return (
+        <li key={status.id} className={status.type}>
+          <StatusMessage msg={status.msg} type={messageTypes[status.type]} time={status.time} />
+        </li>
+      );
+    });
+  }
+
+  return <ul id="status-list">{displayedStatuses}</ul>;
 }
 
 function StatusMessageManager() {
-	return (
-		<Fragment>
-			<div id="post-status">
-				<PostForm />
-			</div>
-			<StatusMessageList />
-		</Fragment>
-	);
+  return (
+    <>
+      <div id="post-status">
+        <PostForm />
+      </div>
+      <StatusMessageList />
+    </>
+  );
 }
 
 createRoot(document.getElementById("react-statusmanager")).render(
-	<StrictMode>
-		<StatusMessageManager />
-	</StrictMode>,
+  <StrictMode>
+    <StatusMessageManager />
+  </StrictMode>,
 );
