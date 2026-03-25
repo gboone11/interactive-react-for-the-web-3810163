7b6b91de69f869a99c72e3a031ac6f630357de8d
gboone11 <gavin.boone@ejgallo.com>
2026-03-25 10:38:12 -0700

    Ch03 06

--- Ch03/wip/directory.jsx
+++ Ch03/wip/directory.jsx
@@ -30,18 +30,44 @@ function People({ people }) {
   );
 }
 
-function Filters() {
+function Filters({
+  currentName,
+  currentTitle,
+  isIntern,
+  setCurrentName,
+  setCurrentTitle,
+  setIsIntern,
+}) {
   const titles = window.LMDirectory.titles;
+
+  const updateName = (event) => {
+    setCurrentName(event.target.value);
+  };
+
+	const updateTitle = (event) => {
+    setCurrentTitle(event.target.value);
+  };
+
+	const updateIntern = (event) => {
+    setIsIntern(event.target.checked);
+  };
 
   return (
     <form action="" id="directory-filters">
       <div className="group">
         <label htmlFor="txt-name">Name:</label>
-        <input name="person_name" value="" placeholder="Name of employee" id="person-name" />
+        <input
+          type="text"
+          name="person_name"
+          placeholder="Name of employee"
+          id="person-name"
+          value={currentName}
+          onChange={updateName}
+        />
       </div>
       <div className="group">
         <label htmlFor="sel-title">Job Title:</label>
-        <select name="sel-title" id="sel-title">
+        <select name="sel-title" id="sel-title" value={currentTitle} onChange={updateTitle}>
           <option value="">- Select -</option>
           {titles.map((title) => (
             <option value={title.key} key={title.key}>
@@ -52,7 +78,7 @@ function Filters() {
       </div>
       <div className="group">
         <label>
-          <input type="checkbox" value="1" /> Intern
+          <input type="checkbox" value="1" checked={isIntern} onChange={updateIntern} /> Intern
         </label>
       </div>
     </form>
@@ -61,13 +87,23 @@ function Filters() {
 
 function Directory() {
   const [people, setPeople] = useState(window.LMDirectory.people);
+  const [currentName, setCurrentName] = useState("");
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [isIntern, setIsIntern] = useState(false);
 
   return (
     <div className="company-dir">
       <h2>Company Directory</h2>
       <p>Learn more about each person at Leaf & Mortar in this company directory.</p>
 
-      <Filters />
+      <Filters
+        currentName={currentName}
+        currentTitle={currentTitle}
+        isIntern={isIntern}
+        setCurrentName={setCurrentName}
+        setCurrentTitle={setCurrentTitle}
+        setIsIntern={setIsIntern}
+      />
 
       <People people={people} />
     </div>
